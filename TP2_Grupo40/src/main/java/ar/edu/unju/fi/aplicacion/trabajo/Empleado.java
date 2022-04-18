fdde255ac3ca89bbc1932e736ce8c592f0b7c6ea
FedericoAgustin <fedeasalinas@gmail.com>
2022-04-17 21:35:55 -0300

    Desarrollo en colaboración TP2_Grupo40

---
+++ TP2_Grupo40/src/main/java/ar/edu/unju/fi/aplicacion/trabajo/Empleado.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.aplicacion.trabajo;
+
+public class Empleado {
+	
+	public String nombre;
+	public String fecha_ingreso;
+	public int Legajo;
+	public String Email;
+	public double Sueldo;
+	public int horas_trabajadas;
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
