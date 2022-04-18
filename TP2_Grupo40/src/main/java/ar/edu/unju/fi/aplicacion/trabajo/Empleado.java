f6587b2ad3c41c736d17171ed494919612489fee
FedericoAgustin <fedeasalinas@gmail.com>
2022-04-17 22:15:59 -0300

    P13 editado

--- TP2_Grupo40/src/main/java/ar/edu/unju/fi/aplicacion/trabajo/Empleado.java
+++ TP2_Grupo40/src/main/java/ar/edu/unju/fi/aplicacion/trabajo/Empleado.java
@@ -8,13 +8,26 @@ public class Empleado {
 	public String Email;
 	public double Sueldo;
 	public int horas_trabajadas;
-	
+	public static int prod;
 	
 	public static void main(String[] args) {
+	  
+		int hora=0;
 		
+	if (hora==160){
 		
+		prod= 160*600;
 		
-
+		System.out.println("Sueldo del empleado:"+ prod);
+	
 	}
-
+	else {
+   
+	 
+	}
+		
+}		
 }
+
+
+
